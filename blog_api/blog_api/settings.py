42712110c4e9584a0dbe1ffc2c9a5c3a1ca957c2
Avinash <avinashnarwariya5@gmail.com>
2025-07-06 23:15:42 +0530

    fixed secret leak

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 DEBUG = True
 ALLOWED_HOSTS = []
 
